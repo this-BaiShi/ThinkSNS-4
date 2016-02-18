7ff917b9a8cb8e1216ad28e31dc2f5f2a3f31b97
Seven <lovevipdsw@outlook.com>
2016-02-18 10:40:52 +0800

    删除冗余~

--- src/Build.php
+++ src/Build.php
@@ -14,7 +14,7 @@ if (!file_exists($file)) {
     echo 'You must set up the project dependencies, run the following commands:', PHP_EOL,
          'curl -sS https://getcomposer.org/installer | php', PHP_EOL,
          'php composer.phar install', PHP_EOL;
-    exit(1);
+    exit;
 }
 
 $loader = include $file;
